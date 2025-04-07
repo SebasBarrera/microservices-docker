65732c485fbba814771005f50ef59960b39834b3
Juan Sebastián Barrera Pulido <47837394+SebasBarrera@users.noreply.github.com>
2025-04-07 00:09:52 +0000

    Project has been initialized

---
+++ microservices-docker/src/main/java/com/eci/arcn/microservices_docker/MicroservicesDockerApplication.java
@@ -0,0 +1,13 @@
+package com.eci.arcn.microservices_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesDockerApplication.class, args);
+	}
+
+}
