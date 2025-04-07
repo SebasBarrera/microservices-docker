1d31c0c23ef8502135b66719c135570201430afd
Juan Sebastián Barrera Pulido <47837394+SebasBarrera@users.noreply.github.com>
2025-04-07 00:16:11 +0000

    Hello world has been created

---
+++ src/main/java/com/eci/arcn/microservices_docker/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.eci.arcn.microservices_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
